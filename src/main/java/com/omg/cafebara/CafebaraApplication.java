03e4b14c36ad0a3fd035fe8a4d0e80ddfa4297ec
PgmJun <chltmdwns96@gmail.com>
2024-03-17 19:49:08 +0900

    [Init] Project init

---
+++ src/main/java/com/omg/cafebara/CafebaraApplication.java
@@ -0,0 +1,13 @@
+package com.omg.cafebara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafebaraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafebaraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/omg/cafebara/CafebaraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omg.cafebara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafebaraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
